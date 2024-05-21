447991c737b4d16be841c0a3612f2a3b3847cb18
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-05-21 10:35:25 +0530

    Create Primitive.java

---
+++ Primitive.java
@@ -0,0 +1,10 @@
+public class Primitive{
+  public static void main(String[] args){
+     int roll =64;
+     char letter='r';
+     float marks= 98.67f;
+     double largedeci= 83838933893.8889;
+     long largeinteger= 73892137894199810019L;
+     boolean check= false;
+   }
+}
