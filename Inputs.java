c2052a05b196be2ee4bdb6f9d82529a3d2cab179
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-05-21 10:32:17 +0530

    Create Inputs.java

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Inputs{
+  public static void main(String[] args){
+    Scanner scc= new Scanner(System.in);
+    System.out.print("Please enter your roll number : ");
+    int roll = sc.nextInt();
+    System.out.println("Your roll number is "+ roll);
+  }
+}
