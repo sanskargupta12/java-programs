a87812d989f0d83183144423ccaf49fb745ab4cd
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-06-14 10:51:16 -0700

    Create CalculateSI.java

---
+++ CalculateSI.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class CalculateSI {
+	public static void main(String args[])
+	{
+		float P = 1, R = 1, T = 1;
+		float SI = (P * T * R) / 100;
+		System.out.println("Simple interest = " + SI);
+	}
+}
