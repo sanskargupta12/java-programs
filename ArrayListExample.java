b947a13e83e63cf3e1e8b0b4a04f5617811c160c
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-07-25 23:52:54 -1200

    Create ArrayListExample.java

---
+++ ArrayListExample.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i)); 
+        }
+        System.out.println(list);
+    }
+}
