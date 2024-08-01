b287aad5082c9c51d7119ff815c4e62619cc1365
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-08-01 00:24:26 -1200

    Create linearSearchElementNotFound.java

---
+++ linearSearchElementNotFound.java
@@ -0,0 +1,22 @@
+public class linearSearchElementNotFound {
+    public static void main(String[] args) {
+	    int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+	    int target = 156;
+	    int ans = linearSearch(nums,target);
+	    System.out.println(ans);
+    }
+	    static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int index = 0; index < arr.length; index++) {
+            // check for element at every index if it is = target
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+}
