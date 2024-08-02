b7211bc1b4ca6303cc71522d73ea3109addc86bc
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2024-08-01 23:45:00 -1200

    Create MAXandMINSearchIn2DArray.java

---
+++ MAXandMINSearchIn2DArray.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class MAXandMINSearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23, 4, 1},
+                {18, 12, 3, 9},
+                {78, 99, 34, 56},
+                {18, 12}
+        };
+         System.out.println(max(arr));
+        System.out.println(Integer.MIN_VALUE);
+    }
+     static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int element : ints) {
+                if (element > max) {
+                    max = element;
+                }
+            }
+        }
+        return max;
+    }
+}
+        
